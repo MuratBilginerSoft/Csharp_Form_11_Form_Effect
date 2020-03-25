f71d2cc2e0c6ea0d2301bb0ca5c3eb39ca5216b3
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:04:56 +0300

    Initial commit

---
+++ Form Effect/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Form_Effect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblX = new System.Windows.Forms.Label();
+            this.LblY = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblW = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblH = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(138, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X:";
+            // 
+            // LblX
+            // 
+            this.LblX.AutoSize = true;
+            this.LblX.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblX.Location = new System.Drawing.Point(52, 18);
+            this.LblX.Name = "LblX";
+            this.LblX.Size = new System.Drawing.Size(21, 22);
+            this.LblX.TabIndex = 2;
+            this.LblX.Text = "0";
+            // 
+            // LblY
+            // 
+            this.LblY.AutoSize = true;
+            this.LblY.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblY.Location = new System.Drawing.Point(52, 55);
+            this.LblY.Name = "LblY";
+            this.LblY.Size = new System.Drawing.Size(21, 22);
+            this.LblY.TabIndex = 4;
+            this.LblY.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(12, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Y:";
+            // 
+            // LblW
+            // 
+            this.LblW.AutoSize = true;
+            this.LblW.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblW.Location = new System.Drawing.Point(278, 55);
+            this.LblW.Name = "LblW";
+            this.LblW.Size = new System.Drawing.Size(21, 22);
+            this.LblW.TabIndex = 8;
+            this.LblW.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(238, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "W:";
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblH.Location = new System.Drawing.Point(278, 18);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(21, 22);
+            this.lblH.TabIndex = 6;
+            this.lblH.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(238, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 22);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "H:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 188);
+            this.Controls.Add(this.LblW);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LblY);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LblX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblX;
+        private System.Windows.Forms.Label LblY;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblW;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form Effect/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Effect
+{
+    public partial class Form1 : Form
+    {
+        #region Parameters
+
+        int x, y;
+
+        int k, y2, m;
+
+        int k1 = 0;
+
+        int r1, r2;
+
+        Point u;
+        
+
+        #endregion
+
+        #region Definition
+
+        Random r = new Random();
+
+        #endregion
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            lblH.Text = this.Height.ToString();
+            LblW.Text = this.Width.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 100;
+
+            u = this.Location;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            k1++;
+
+            k = r.Next(0, 256);
+            y2 = r.Next(0, 256);
+            m = r.Next(0, 256);
+
+
+
+            this.BackColor = Color.FromArgb(k, y2, m);
+
+            label1.Text = k1.ToString();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            r1 = r.Next(1, 10);
+            r2 = r.Next(1, 10);
+
+            u.X += r1;
+            u.Y += r2;
+
+            LblX.Text = u.X.ToString();
+            LblY.Text = u.Y.ToString();
+
+            this.Location = new Point(u.X, u.Y);
+
+            x = r.Next(1, 10);
+            y = r.Next(1, 10);
+
+            this.Height += x;
+            this.Width += y;
+
+            lblH.Text = this.Height.ToString();
+            LblW.Text = this.Width.ToString();
+
+            if (this.Height>=400 && this.Width>=600)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+    }
+}
